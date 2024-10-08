617899c3da3084df4020f2ad832113c2bc25678f
lepolesivan <ivansomo27@gmail.com>
2024-10-08 12:29:21 +0200

    Assignation et modification de l'assignation pour les parents

--- src/Models/Task.php
+++ src/Models/Task.php
@@ -17,9 +17,10 @@ class Task
     protected ?int $id_user;
     protected ?string $status;
     protected ?string $pseudo;
+    protected ?int $idKid;
 
 
-    public function __construct(?int $id, ?string $title, ?string $content, ?string $creation_date, ?string $start_task, ?string $stop_task, ?int $point, ?int $id_user, ?string $status, ?string $pseudo)
+    public function __construct(?int $id, ?string $title, ?string $content, ?string $creation_date, ?string $start_task, ?string $stop_task, ?int $point, ?int $id_user, ?string $status, ?string $pseudo, ?int $idKid)
     {
         $this->id = $id;
         $this->title = $title;
@@ -31,6 +32,7 @@ class Task
         $this->id_user = $id_user;
         $this->status = $status;
         $this->pseudo = $pseudo;
+        $this->idKid = $idKid;
     }
 
     public function addTask(): bool
@@ -55,7 +57,7 @@ class Task
         $resultFetch = $statement->fetchAll(PDO::FETCH_ASSOC);
         $tasks = [];
         foreach ($resultFetch as $row) {
-            $task = new Task($row['id'], $row['title'], null, null, $row['start_task'], $row['stop_task'], null, null, null, null);
+            $task = new Task($row['id'], $row['title'], null, null, $row['start_task'], $row['stop_task'], null, null, null, null, null);
             $tasks[] = $task;
         }
         return $tasks;
@@ -76,7 +78,7 @@ class Task
         $resultFetch = $statement->fetchAll(PDO::FETCH_ASSOC);
         $tasks = [];
         foreach ($resultFetch as $row) {
-            $task = new Task($row['id'], $row['title'], null, null, $row['start_task'], $row['stop_task'], null, null, null, $row['pseudo']);
+            $task = new Task($row['id'], $row['title'], null, null, $row['start_task'], $row['stop_task'], null, null, null, $row['pseudo'], null);
             $tasks[] = $task;
         }
         return $tasks;
@@ -90,7 +92,7 @@ class Task
         $statement->execute([$this->id]);
         $row = $statement->fetch(PDO::FETCH_ASSOC);
         if ($row) {
-            return new Task($row['id'], $row['title'], $row['content'], $row['creation_date'], $row['start_task'], $row['stop_task'], $row['point'], $row['id_user'], $row['status'], $row['pseudo']);
+            return new Task($row['id'], $row['title'], $row['content'], $row['creation_date'], $row['start_task'], $row['stop_task'], $row['point'], $row['id_user'], $row['status'], $row['pseudo'], null);
         } else {
             return null;
         }
@@ -122,6 +124,22 @@ class Task
         return $statement->execute([$this->id]);
     }
 
+    public function addTodo()
+    {
+        $pdo = DataBase::getConnection();
+        $sql = "INSERT INTO `todo` (`status`, `id_user`, `id_task`) VALUES (?,?,?)";
+        $statement = $pdo->prepare($sql);
+        return $statement->execute([$this->status, $this->idKid, $this->id]);
+    }
+
+    public function updateTodo()
+    {
+        $pdo = DataBase::getConnection();
+        $sql = "UPDATE `todo` SET `todo`.`status` = ?, `todo`.`id_user` = ? WHERE `todo`.`id_task`= ?";
+        $statement = $pdo->prepare($sql);
+        return $statement->execute([$this->status, $this->idKid, $this->id]);
+    }
+    
     public function getId(): ?int
     {
         return $this->id;
@@ -168,6 +186,11 @@ class Task
     public function getPseudo(): ?string
     {
         return $this->pseudo;
+    }
+
+    public function getIdKid(): ?int
+    {
+        return $this->idKid;
     }
 
     public function setId(?int $id): static
@@ -227,6 +250,12 @@ class Task
     public function setPseudo(?string $pseudo): static
     {
         $this->pseudo = $pseudo;
+        return $this;
+    }
+
+    public function setIdKid(?int $idKid): static
+    {
+        $this->idKid = $idKid;
         return $this;
     }
 }
